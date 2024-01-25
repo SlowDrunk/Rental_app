ea65c76628513ab010fffc15fd8dbeb5befb2cd7
SlowDrunk <2359944605@qq.com>
2024-01-25 21:52:51 +0800

    feat:新增城市列表渲染

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@/assets/css/reset.css';
 import '@/assets/css/index.css';
+import 'react-virtualized/styles.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- src/views/Citylist/index.tsx
+++ src/views/Citylist/index.tsx
@@ -2,6 +2,7 @@ import { NavBar } from 'antd-mobile'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { getCityList, getHotCity } from '@/api'
+import { List, AutoSizer } from 'react-virtualized'
 
 interface CityItem {
   label: string;
@@ -21,7 +22,12 @@ interface CityList {
  * @returns {CityList} - 格式化后的数据
  */
 
-const groupCitiesByInitial = (data: CityItem[]): CityList => {
+interface ResultData {
+  newCityList: CityList;
+  cityIndex: string[];
+}
+
+const groupCitiesByInitial = (data: CityItem[]): ResultData => {
   const result: CityList = {};
   data.forEach((city) => {
     const key = city.short[0];
@@ -30,38 +36,104 @@ const groupCitiesByInitial = (data: CityItem[]): CityList => {
     }
     result[key].push(city);
   });
-  return result;
+  if (localStorage.getItem('cityInfo')) {
+    result['#'] = [JSON.parse(localStorage.getItem('cityInfo') || '{}')]
+  }
+  return {
+    newCityList: result,
+    cityIndex: Object.keys(result).sort(),
+  };
 };
 
+/**
+ * 处理字母索引
+ *
+ * @param {string} letter - 传入的字符
+ * @returns {string} 返回的字符
+ */
+const formatterLetter = (letter: string) => {
+  switch (letter) {
+    case '#':
+      return '当前定位'
+    case 'hot':
+      return '热门城市'
+    default:
+      return letter.toUpperCase()
+  }
+}
 
+function swapArrayElements(arr:any[], index1:number, index2:number) {
+  arr[index1] = arr.splice(index2, 1, arr[index1])[0];
+  return arr;
+}
 
 export default function Citylist() {
   const navigate = useNavigate()
-  const [cityList, setCityList] = useState<CityItem[]>([])
+  const [cityList, setCityList] = useState<CityList>({})
+  const [cityIndex, setCityIndex] = useState<string[]>([])
   // TODO:获取城市列表数据
   useEffect(() => {
     getCityList(1).then(async (res) => {
-      setCityList(res.data.body)
-      let newCityList = groupCitiesByInitial(res.data.body)
-      const cityIndex = ['hot', ...Object.keys(newCityList).sort()]
-      newCityList['hot'] = (await getHotCity()).data.body
-      console.log(newCityList, cityIndex)
+      const { newCityList, cityIndex } = groupCitiesByInitial(res.data.body)
+      getHotCity().then((res) => {
+        newCityList['hot'] = res.data.body
+        cityIndex.unshift('hot')
+        setCityList(newCityList)
+        setCityIndex(swapArrayElements(cityIndex, 0, 1))
+      })
     })
   }, [])
 
+  const getRowHeight = ({ index }: any) => {
+    return 36 + cityList[cityIndex[index]].length * 50
+  }
+  function rowRenderer({
+    key,
+    index,
+    isScrolling,
+    isVisible,
+    style,
+  }: any) {
+    return (
+      <div key={key} style={style} className='flex flex-col'>
+        <div className='text-[14px] py-[10px] px-[15px] text-[#999999]'>{formatterLetter(cityIndex[index])}</div>
+        {
+          cityList[cityIndex[index]]?.map((item: CityItem) => {
+            return (
+              <div key={item.value} className='w-full h-[50px] px-[15px] text-[16px] text-[#333333] bg-white cursor-pointer leading-[50px]' style={{ borderBottom: '1px solid #ececec' }}>{item.label}</div>
+            )
+          })
+        }
+      </div>
+    );
+  }
+
+
   return (
-    <div>
+    <div className='h-full'>
       {/* 顶部nav */}
-      <NavBar
-        style={{
-          '--height': '36px',
-          '--border-bottom': '1px #eee solid',
-          background: '#ececec'
-        }}
-        onBack={() => navigate(-1)}
-      >
-        城市列表
-      </NavBar>
+      <div className='fixed top-0 w-full z-20'>
+        <NavBar
+          style={{
+            '--height': '36px',
+            '--border-bottom': '1px #eee solid',
+            background: '#ececec'
+          }}
+          onBack={() => navigate(-1)}
+        >
+          城市选择
+        </NavBar>
+      </div>
+      <AutoSizer className='mt-[36px]'>{({ height, width }) =>
+        <List
+          width={width}
+          height={height}
+          rowCount={Object.keys(cityList).length}
+          rowHeight={getRowHeight}
+          rowRenderer={rowRenderer}
+        />
+      }
+      </AutoSizer>
     </div>
   )
 }
